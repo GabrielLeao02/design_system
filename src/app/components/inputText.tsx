4a744e3f45b578ea200a76e875e57c8499232e3b
GabrielLeao <90697922+GabrielLeao02@users.noreply.github.com>
2024-03-07 11:48:17 -0300

    ajustes nas condições do operador ternário do Icon

--- src/app/components/inputText.tsx
+++ src/app/components/inputText.tsx
@@ -8,16 +8,19 @@ interface InputTextProps {
 
 export function InputText({ title, width, icon: Icon }: InputTextProps) {
 	return (
-		<>
-			<div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
-				<input
-					type='text'
-					placeholder={title}
-					style={{ width: width }}
-				/>
-				{Icon ? <Icon></Icon> : ''}
-			</div>
-		</>
+		<div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
+			<input
+				type='text'
+				placeholder={title}
+				style={{
+					width: width,
+					paddingRight: '20px',
+					height: '20px',
+					lineHeight: '20px',
+				}}
+			/>
+			{Icon && <Icon style={{ fontSize: '15px', marginLeft: '-30px' }} />}
+		</div>
 	);
 }
 
